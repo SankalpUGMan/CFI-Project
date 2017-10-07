f6d1da9fbb9e665913d8674b72a478f268f32eee
SankalpUGMan <sankalp_saoji@rediffmail.com>
2017-10-07 23:32:06 +0530

    done

--- dashboard.php
+++ dashboard.php
@@ -14,7 +14,7 @@ Your Email Address is <?php echo $_POST['email'];?>
 <br>
 </center>
 <script>
-    window.location="https://github.com/SankalpUGMan/CFI-Project/blob/master/map.html"; 
+    window.location="https://sankalpugman.github.io/CFI-Project/map.html"; 
 </script>
 </body>
 </html>
